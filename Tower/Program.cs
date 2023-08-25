5b6ecf48a68dbc00a743312d89fd7cc8907c11d3
Danilo de Arruda Kuhlmann <danilo.kuhlmann@hotmail.com>
2023-08-25 01:02:02 -0300

    Adição da biblioteca swagger para API

--- Tower/Program.cs
+++ Tower/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using Tower.Classes;
 using Tower.Database;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,43 +19,116 @@ builder.Services.AddAuthorization(options =>
         .RequireAuthenticatedUser()
         .Build();
 });
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+});
 builder.Services.AddAuthentication().AddCookie(options =>
 {
-	options.LoginPath = "/Users/login";
-	options.AccessDeniedPath = "/Users/AccessDenied";
-	options.Cookie.Name = ".AspNet.Sistema.SharedCookie";
-	options.Cookie.Path = "/";
-	options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-	options.Cookie.SameSite = SameSiteMode.Lax;
-	options.Cookie.HttpOnly = true;
-	options.Cookie.IsEssential = true;
+    options.LoginPath = "/Users/login";
+    options.AccessDeniedPath = "/Users/AccessDenied";
+    options.Cookie.Name = ".AspNet.Sistema.SharedCookie";
+    options.Cookie.Path = "/";
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.SameSite = SameSiteMode.Lax;
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+}).AddJwtBearer(x =>
+{
+    var key = SecurityClass.GenerateKey();
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key.Key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+    };
 });
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("JWT", policy =>
+    {
+        policy.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+        policy.RequireAuthenticatedUser();
+    });
+});
+builder.Services.AddSwaggerGen(c =>
+{
+    // Habilita a documentação de anotaçoes do Swagger
+    c.EnableAnnotations();
 
+    // Define a informação básica da API
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API de consultas Mister Multas",
+    });
+
+    // Define o esquema de segurança do token JWT
+    var securySchema = new OpenApiSecurityScheme
+    {
+        Description = "Token de acesso, coloque no cabeçalho da chamada o token de acesso da mesma maneira que foi gerado na pagina",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Name = "Token",
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Token"
+        },
+    };
+    c.AddSecurityDefinition("Token", securySchema);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                { securySchema, new[] { "Token" } }
+            });
+
+    // Configuraçãoo para evitar conflitos de rotas
+    c.ResolveConflictingActions(x => x.First());
+
+    // Habilita as anotações para herança e polimorfismo
+    c.EnableAnnotations(enableAnnotationsForInheritance: true, enableAnnotationsForPolymorphism: true);
+
+    // Configuração para incluir a documentação XML da API
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 builder.Services.AddDbContext<BDContext>(
 dbContextOptions => dbContextOptions
-				.UseMySql(BDContext.DefaultConnection, ServerVersion.AutoDetect(BDContext.DefaultConnection))
-				.EnableDetailedErrors()
-		);
+                .UseMySql(BDContext.DefaultConnection, ServerVersion.AutoDetect(BDContext.DefaultConnection))
+                .EnableDetailedErrors()
+        );
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSwagger(c =>
+{
+    c.RouteTemplate = "swagger/{documentName}/swagger.json";
+});
+app.UseSwaggerUI(
+    options =>
+    {
+        options.DocumentTitle = "API de consulta";
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    }
+);
 app.UseAuthorization();
-
 app.MapControllerRoute(
-	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
